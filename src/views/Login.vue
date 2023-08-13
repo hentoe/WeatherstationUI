15532e5c9e73b5b4009ae0bb149c3077dd54bbb4
hentoe <hendriktoedter@gmx.de>
2023-08-13 22:53:44 +0200

    Add link to register page.

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -66,7 +66,7 @@
       <p class="mt-10 text-center text-sm text-gray-500">
         Not a member?
         {{ ' ' }}
-        <a href="#" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
+        <a href="/register" class="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
           >Register here.</a
         >
       </p>
