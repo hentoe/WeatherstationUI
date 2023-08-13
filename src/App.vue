343e32ec89e62a0a822b3583a55ac8df92c78e51
hentoe <hendriktoedter@gmx.de>
2023-08-13 22:51:03 +0200

    Add navbar for unauthenticated state.

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import Navbar from './components/Navbar.vue'
+import NavbarNoAuth from './components/NavbarNoAuth.vue'
 </script>
 
 <template>
-  <Navbar />
+  <!-- Navbar when logged out -->
+  <NavbarNoAuth />
+  <!-- Navbar when logged in -->
+  <!-- <Navbar /> -->
 
   <RouterView />
 </template>
