a8eeb644b41a8e493ba26538704698af08f0568b
hentoe <hendriktoedter@gmx.de>
2023-08-14 10:33:50 +0200

    Add name to register view.

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -15,6 +15,18 @@
       <!-- Form -->
       <form class="space-y-6" action="#" method="POST">
         <div>
+          <label for="name" class="block text-sm font-medium leading-6 text-gray-900">Name</label>
+          <div class="mt-2">
+            <input
+              id="name"
+              name="name"
+              type="text"
+              required
+              class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+            />
+          </div>
+        </div>
+        <div>
           <label for="email" class="block text-sm font-medium leading-6 text-gray-900"
             >Email address</label
           >
