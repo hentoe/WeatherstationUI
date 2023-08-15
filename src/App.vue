49c6c424ebc63f648aeba432e37ef72590bb787a
hentoe <hendriktoedter@gmx.de>
2023-08-15 10:50:06 +0200

    Refactor

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,21 @@
-<script>
+<script setup>
 import { RouterLink, RouterView } from 'vue-router'
 import { useAuthStore } from './stores/auth.store'
 import axios from 'axios'
 import Navbar from './components/Navbar.vue'
 import NavbarNoAuth from './components/NavbarNoAuth.vue'
 
-export default {
-  name: 'App',
-  beforeCreate() {
-    const authStore = useAuthStore()
-    authStore.setToken(authStore.token)
+const authStore = useAuthStore()
+authStore.setToken(authStore.token)
 
-    axios.defaults.headers.common['Authorization'] = authStore.isAuthenticated
-      ? `Token ${authStore.token}`
-      : ''
-  }
-}
+axios.defaults.headers.common['Authorization'] = authStore.isAuthenticated
+  ? `Token ${authStore.token}`
+  : ''
 </script>
 
 <template>
-  <!-- Navbar when logged out -->
-  <NavbarNoAuth />
-  <!-- Navbar when logged in -->
-  <!-- <Navbar /> -->
+  <NavbarNoAuth v-if="!authStore.isAuthenticated" />
+  <Navbar v-else />
 
   <RouterView />
 </template>
