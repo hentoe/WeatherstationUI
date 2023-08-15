13109f1e6aefceeae86a4083cc1e24555e10cdd2
hentoe <hendriktoedter@gmx.de>
2023-08-15 10:39:35 +0200

    Put token in browser storage.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,21 @@
-<script setup>
+<script>
 import { RouterLink, RouterView } from 'vue-router'
+import { useAuthStore } from './stores/auth.store'
+import axios from 'axios'
 import Navbar from './components/Navbar.vue'
 import NavbarNoAuth from './components/NavbarNoAuth.vue'
+
+export default {
+  name: 'App',
+  beforeCreate() {
+    const authStore = useAuthStore()
+    authStore.setToken(authStore.token)
+
+    axios.defaults.headers.common['Authorization'] = authStore.isAuthenticated
+      ? `Token ${authStore.token}`
+      : ''
+  }
+}
 </script>
 
 <template>
--- src/stores/auth.store.js
+++ src/stores/auth.store.js
@@ -2,17 +2,19 @@ import { defineStore } from 'pinia'
 
 export const useAuthStore = defineStore('auth', {
   state: () => ({
-    token: null,
-    isAuthenticated: false
+    token: localStorage.getItem('token') || '',
+    isAuthenticated: !!localStorage.getItem('token')
   }),
   actions: {
     setToken(token) {
       this.token = token
       this.isAuthenticated = true
+      localStorage.setItem('token', token)
     },
     clearToken() {
       this.token = null
       this.isAuthenticated = false
+      localStorage.removeItem('token')
     }
   }
 })
