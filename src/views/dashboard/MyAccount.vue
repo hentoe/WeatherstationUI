72d1d54994d300015ac2c694c1a5c003b040ad11
hentoe <hendriktoedter@gmx.de>
2023-08-15 14:27:30 +0200

    Implemented account details page.

--- src/views/dashboard/MyAccount.vue
+++ src/views/dashboard/MyAccount.vue
@@ -4,6 +4,49 @@
       <h1 class="text-3xl font-bold tracking-tight text-gray-900">My Account</h1>
     </div>
   </header>
+
+  <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+    <div class="px-4 sm:px-0">
+      <p class="mt-1 max-w-2xl text-sm leading-6 text-gray-500">Personal details.</p>
+    </div>
+    <div class="mt-6 border-t border-gray-100">
+      <dl class="divide-y divide-gray-100">
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt class="text-sm font-medium leading-6 text-gray-900">Full name</dt>
+          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {{ userData.name }}
+          </dd>
+        </div>
+
+        <div class="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
+          <dt class="text-sm font-medium leading-6 text-gray-900">Email address</dt>
+          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
+            {{ userData.email }}
+          </dd>
+        </div>
+      </dl>
+    </div>
+  </div>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+
+const userData = ref({
+  name: '',
+  email: ''
+})
+
+onMounted(async () => {
+  try {
+    const response = await axios.get('/api/user/me')
+    userData.value = {
+      name: response.data.name,
+      email: response.data.email
+    }
+  } catch (error) {
+    console.error('Error fetching user data:', error)
+  }
+})
+</script>
