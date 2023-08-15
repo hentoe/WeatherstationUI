66620389d5e8efb0239cc4dd4929f53a51407751
hentoe <hendriktoedter@gmx.de>
2023-08-15 13:22:46 +0200

    Removed unnecessary stuff.

--- src/components/NavbarNoAuth.vue
+++ src/components/NavbarNoAuth.vue
@@ -2,18 +2,6 @@
   <Disclosure as="nav" class="bg-gray-800" v-slot="{ open }">
     <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
       <div class="relative flex h-16 items-center justify-between">
-        <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
-          <!-- Mobile menu button-->
-          <DisclosureButton
-            class="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-          >
-            <span class="absolute -inset-0.5" />
-            <span class="sr-only">Open main menu</span>
-            <Bars3Icon v-if="!open" class="block h-6 w-6" aria-hidden="true" />
-            <XMarkIcon v-else class="block h-6 w-6" aria-hidden="true" />
-          </DisclosureButton>
-        </div>
-
         <div class="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start">
           <div class="flex flex-shrink-0 items-center">
             <img
@@ -46,8 +34,7 @@
 </template>
 
 <script setup>
-import { Disclosure, DisclosureButton } from '@headlessui/vue'
-import { Bars3Icon, BellIcon, XMarkIcon } from '@heroicons/vue/24/outline'
+import { Disclosure } from '@headlessui/vue'
 
 const navigation = [
   { name: 'Login', href: 'login' },
