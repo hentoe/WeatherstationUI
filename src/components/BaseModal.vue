93b731d3f9dcaca291a10e6ccea2c42092bc7cc7
hentoe <hendriktoedter@gmx.de>
2023-08-16 14:03:01 +0200

    Add reusable modal.

---
+++ src/components/BaseModal.vue
@@ -0,0 +1,26 @@
+<template>
+  <div
+    v-show="modalActive"
+    class="absolute w-full bg-black bg-opacity-30 h-screen top-0 left-0 flex justify-center px-8"
+  >
+    <div v-if="modalActive" class="p-4 bg-white self-start mt-32 max-w-screen-md">
+      <slot></slot>
+      <button
+        @click="$emit('close-modal')"
+        class="trounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
+      >
+        Close
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineEmits(['close-modal'])
+defineProps({
+  modalActive: {
+    type: Boolean,
+    default: false
+  }
+})
+</script>
--- src/views/dashboard/Sensors.vue
+++ src/views/dashboard/Sensors.vue
@@ -7,110 +7,27 @@
   <div class="fixed inset-0 flex items-center justify-center">
     <button
       type="button"
-      @click="openModal"
+      @click="toggleModal"
       class="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
     >
-      Open dialog
+      Open Modal
     </button>
   </div>
 
   <!-- Delete Modal -->
-  <!-- <DeleteModal :show="open" :closeModal="closeModal" /> -->
-
-  <TransitionRoot as="template" :show="open">
-    <Dialog as="div" class="relative z-10" @close="open = false">
-      <TransitionChild
-        as="template"
-        enter="ease-out duration-300"
-        enter-from="opacity-0"
-        enter-to="opacity-100"
-        leave="ease-in duration-200"
-        leave-from="opacity-100"
-        leave-to="opacity-0"
-      >
-        <div class="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
-      </TransitionChild>
-
-      <div class="fixed inset-0 z-10 overflow-y-auto">
-        <div
-          class="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0"
-        >
-          <TransitionChild
-            as="template"
-            enter="ease-out duration-300"
-            enter-from="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-            enter-to="opacity-100 translate-y-0 sm:scale-100"
-            leave="ease-in duration-200"
-            leave-from="opacity-100 translate-y-0 sm:scale-100"
-            leave-to="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
-          >
-            <DialogPanel
-              class="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg"
-            >
-              <div class="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
-                <div class="sm:flex sm:items-start">
-                  <div
-                    class="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10"
-                  >
-                    <ExclamationTriangleIcon class="h-6 w-6 text-red-600" aria-hidden="true" />
-                  </div>
-                  <div class="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
-                    <DialogTitle as="h3" class="text-base font-semibold leading-6 text-gray-900"
-                      >Delete object</DialogTitle
-                    >
-                    <div class="mt-2">
-                      <p class="text-sm text-gray-500">
-                        Are you sure you want to delete this object? It will be permanently removed.
-                        This action cannot be undone.
-                      </p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div class="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
-                <button
-                  type="button"
-                  class="inline-flex w-full justify-center rounded-md bg-red-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-500 sm:ml-3 sm:w-auto"
-                  @click="handleDelete"
-                >
-                  Delete
-                </button>
-                <button
-                  type="button"
-                  class="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
-                  @click="closeModal"
-                  ref="cancelButtonRef"
-                >
-                  Cancel
-                </button>
-              </div>
-            </DialogPanel>
-          </TransitionChild>
-        </div>
-      </div>
-    </Dialog>
-  </TransitionRoot>
+  <BaseModal :modalActive="modalActive" @close-modal="toggleModal">
+    <h1>Hello from Modal.</h1></BaseModal
+  >
 </template>
 
 <script setup>
 import { ref } from 'vue'
+import BaseModal from '../../components/BaseModal.vue'
 
-import { Dialog, DialogPanel, DialogTitle, TransitionChild, TransitionRoot } from '@headlessui/vue'
-import { ExclamationTriangleIcon } from '@heroicons/vue/24/outline'
+// Modal stuff.
+const modalActive = ref(false)
 
-const open = ref(false)
-const itemId = ref(null)
-
-function openModal() {
-  console.log('Open modal')
-  open.value = true
-}
-function closeModal() {
-  console.log('Close modal')
-  open.value = false
-}
-function handleDelete() {
-  console.log('Delete function')
-  open.value = false
+const toggleModal = () => {
+  modalActive.value = !modalActive.value
 }
 </script>
