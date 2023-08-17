c6210cedb1f525a2f38c149f865307301aaae7dc
hentoe <hendriktoedter@gmx.de>
2023-08-17 10:20:34 +0200

    Restyled page.

--- src/views/dashboard/Locations.vue
+++ src/views/dashboard/Locations.vue
@@ -1,71 +1,51 @@
 <template>
-  <header class="bg-white shadow">
+  <div>
+    <header class="bg-white shadow">
+      <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Locations</h1>
+      </div>
+    </header>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <h1 class="text-3xl font-bold tracking-tight text-gray-900">Locations</h1>
-    </div>
-  </header>
-  <main>
-    <div class="mx-auto max-w-xl px-4 py-6 sm:px-6 lg:px-8">
-      <ul role="list" class="divide-y divide-gray-100">
-        <!-- Static row with UI components-->
-        <li class="flex justify-between gap-x-6 py-5">
-          <div class="flex min-w-0 gap-x-4">
-            <div class="min-w-0 flex-auto">
-              <div class="text-sm font-semibold leading-6 text-gray-900">
-                <div
-                  class="rounded-md bg-black bg-opacity-20 px-4 py-2 text-sm font-medium text-white hover:bg-opacity-30 focus:outline-none focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75"
-                >
-                  <PlusIcon class="h-6 w-6" />
-                </div>
-              </div>
-            </div>
-          </div>
-          <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <Switch
-              v-model="enabled"
-              :class="enabled ? 'bg-blue-600' : 'bg-gray-200'"
-              class="relative inline-flex h-6 w-11 items-center rounded-full"
-            >
-              <span class="sr-only">Assigned only</span>
-              <span
-                :class="enabled ? 'translate-x-6' : 'translate-x-1'"
-                class="inline-block h-4 w-4 transform rounded-full bg-white transition"
-              />
-            </Switch>
-          </div>
-        </li>
-        <!-- Content -->
+      <!-- Add Location Form -->
+      <form @submit.prevent="addLocation">
+        <div class="mb-4">
+          <label class="block text-sm font-medium text-gray-700">Location Name</label>
+          <input v-model="newLocationName" type="text" class="mt-1 p-2 w-full border rounded-md" />
+        </div>
+        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md">
+          Add Location
+        </button>
+      </form>
+
+      <!-- Location List -->
+      <ul class="mt-6">
         <li
           v-for="location in locations"
-          :key="location.name"
-          class="flex justify-between gap-x-6 py-5"
+          :key="location.id"
+          class="flex items-center justify-between py-2 border-t"
         >
-          <div class="flex min-w-0 gap-x-4">
-            <div class="min-w-0 flex-auto">
-              <p class="text-sm font-semibold leading-6 text-gray-900">{{ location.name }}</p>
-            </div>
-          </div>
-          <div class="hidden shrink-0 sm:flex sm:flex-col sm:items-end">
-            <button class="cursor-pointer" @click="openModal">
+          <span class="text-lg">{{ location.name }}</span>
+          <div>
+            <button @click="updateLocation(location.id)" class="mr-2">
               <WrenchIcon class="h-6 w-6" />
             </button>
-
-            <button class="cursor-pointer" @click="toggleModal(location.id)">
+            <button @click="toggleModal(location.id)">
               <TrashIcon class="h-6 w-6" />
             </button>
           </div>
         </li>
       </ul>
     </div>
-  </main>
-  <!-- Modal -->
-  <DeleteModal
-    :modalActive="modalActive"
-    @close-modal="toggleModal"
-    @handle-delete="handleDelete"
-    modalHeadline="Delete location?"
-    deleteMessage="Are you sure you want to delete this location? This action cannot be undone!"
-  />
+
+    <!-- Delete Modal -->
+    <DeleteModal
+      :modalActive="modalActive"
+      @close-modal="toggleModal"
+      @handle-delete="handleDelete"
+      modalHeadline="Delete location?"
+      deleteMessage="Are you sure you want to delete this location? This action cannot be undone!"
+    />
+  </div>
 </template>
 
 <script setup>
@@ -100,6 +80,10 @@ const fetchLocations = async (assignedOnly) => {
     console.error('Error fetching locations:', error)
   }
 }
+
+// Add location.
+const newLocationName = ref('')
+const addLocation = () => {}
 
 // Delete Modal.
 const modalActive = ref(false)
