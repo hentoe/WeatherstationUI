8b139377d8850001c2b600df34b10036847f8926
hentoe <hendriktoedter@gmx.de>
2023-11-16 13:50:48 +0100

    Changed color of button

--- src/views/dashboard/Sensors.vue
+++ src/views/dashboard/Sensors.vue
@@ -8,9 +8,7 @@
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <!-- Add Sensor -->
       <RouterLink :to="{ name: 'AddSensor' }">
-        <button type="submit" class="px-4 py-2 bg-blue-500 text-white rounded-md">
-          Add Sensor
-        </button>
+        <button type="submit" class="px-4 py-2 bg-black text-white rounded-md">Add Sensor</button>
       </RouterLink>
 
       <!-- Sensor List -->
