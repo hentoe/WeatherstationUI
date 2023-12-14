202e8986172a983c7b397dcbe7581403b9d8d40b
hentoe <hendriktoedter@gmx.de>
2023-12-14 14:03:20 +0100

    Fix dashboard layout.

--- src/views/dashboard/DashboardView.vue
+++ src/views/dashboard/DashboardView.vue
@@ -5,7 +5,7 @@
     </div>
   </header>
   <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-    <div class="m-4 flex flex-wrap">
+    <div class="m-4 flex flex-wrap justify-center">
       <LocationCard
         v-for="location in weatherstationStore.locations"
         :key="location.id"
