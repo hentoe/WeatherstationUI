333ed798e4b23c0b80d646a9e848bb55f05f52f2
hentoe <hendriktoedter@gmx.de>
2024-09-08 13:04:28 +0200

    Remove dummy settings

--- src/components/SettingsGeneral.vue
+++ src/components/SettingsGeneral.vue
@@ -1,8 +1,6 @@
 <template>
   <SettingsProfile />
-  <SettingsLanguage />
 </template>
 <script setup>
 import SettingsProfile from '../components/SettingsProfile.vue'
-import SettingsLanguage from '../components/SettingsLanguage.vue'
 </script>
